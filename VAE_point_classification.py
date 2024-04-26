6d11c900e3dbb3dd2b3d37e57a47a9582ead8d72
Ryan Shar <ryan.shar01@gmail.com>
2024-04-26 15:53:27 -0700

    refactor to seperate code for training

--- VAE_point_classification.py
+++ VAE_point_classification.py
@@ -2,66 +2,10 @@ import pandas as pd
 import torch
 import numpy as np
 
-from tqdm import tqdm
-from auto_encoder import AutoEncoder 
 from sklearn.preprocessing import StandardScaler
+from auto_encoder import AutoEncoder 
+from training_utils import train
 
-# Creating random noise in a row
-def create_random(n):
-    return torch.tensor(np.random.normal(loc = 0, scale = 1, size = n)).float()
-
-# Masking df with given value 
-def mask_features(df, n, value, random):
-    df_copy = df.clone()
-    
-    for index, row in enumerate(df_copy):
-        
-        idx = torch.randperm(row.size(0))[:n]
-        print("index: " + str(idx))
-        if random == True:
-            row[idx] = create_random(n = n)
-        else: 
-            row[idx] = value
-        
-    return df_copy
-
-# Training process
-def train(autoencoder: AutoEncoder, masked_data: pd.DataFrame, unmasaked_data: pd.DataFrame, epochs=100):
-    opt = torch.optim.Adam(autoencoder.parameters())
-    batch_size = 10
-    n, d = unmasaked_data.shape
-
-    avg_los_epoch = []
-    for epoch in tqdm(range(epochs)):
-        # x = x.to(device) # To format tensors for the GPU
-        # Potentially try torch.nn.BCEWithLogitsLoss()
-        
-        losses = []
-
-        perm_ordering = torch.randperm(n) # potentially control for randomness here
-        for i in range(0, n, batch_size):
-            curr_indices = perm_ordering[i:i+batch_size]
-            X_mask_batch = masked_data[curr_indices]
-            X_unmask_batch = masked_data[curr_indices]
-
-            opt.zero_grad()
-            autoencoder.encoder.kl = 0
-
-            X_hat = autoencoder(X_mask_batch)
-            loss = ((X_unmask_batch - X_hat)**2).sum() + autoencoder.encoder.kl
-            loss = loss / batch_size
-            # loss = ((X_unmask_batch - X_hat)**2).sum() 
-            losses.append(loss.item())
-
-            loss.backward()
-            opt.step()
-    
-        avg_los_epoch.append(sum(losses)/len(losses))
-            
-    # print(avg_los_epoch)
-    print(f"Staring avg losses: {avg_los_epoch[0]}, {avg_los_epoch[1]}, {avg_los_epoch[2]}")
-    print(f"Ending avg loss: {avg_los_epoch[-1]}, {avg_los_epoch[-2]}, {avg_los_epoch[-3]}")
-    return autoencoder
 
 if __name__ == "__main__":
     # torch.manual_seed(42)
@@ -101,8 +45,8 @@ if __name__ == "__main__":
     fraud_model = AutoEncoder(input_dim, latent_dim)
     not_fraud_model = AutoEncoder(input_dim, latent_dim)
 
-    train(fraud_model, X_fraud_tensor, X_fraud_tensor, epochs=500)
-    train(not_fraud_model, X_not_fraud_tensor, X_not_fraud_tensor, epochs=500)
+    fraud_model = train(fraud_model, X_fraud_tensor, X_fraud_tensor, epochs=500)
+    not_fraud_model = train(not_fraud_model, X_not_fraud_tensor, X_not_fraud_tensor, epochs=500)
 
     print("Evaluating non fraud reconstruction")
     for i in range(3):
---
+++ training_utils.py
@@ -0,0 +1,63 @@
+import torch
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+from auto_encoder import AutoEncoder
+
+# Creating random noise in a row
+def create_random(n):
+    return torch.tensor(np.random.normal(loc = 0, scale = 1, size = n)).float()
+
+# Masking df with given value 
+def mask_features(df, n, value, random):
+    df_copy = df.clone()
+    
+    for index, row in enumerate(df_copy):
+        
+        idx = torch.randperm(row.size(0))[:n]
+        print("index: " + str(idx))
+        if random == True:
+            row[idx] = create_random(n = n)
+        else: 
+            row[idx] = value
+        
+    return df_copy
+
+# Training process
+def train(autoencoder: AutoEncoder, masked_data: pd.DataFrame, unmasaked_data: pd.DataFrame, epochs=100):
+    opt = torch.optim.Adam(autoencoder.parameters())
+    batch_size = 10
+    n, d = unmasaked_data.shape
+
+    avg_los_epoch = []
+    for epoch in tqdm(range(epochs)):
+        # x = x.to(device) # To format tensors for the GPU
+        # Potentially try torch.nn.BCEWithLogitsLoss()
+        
+        losses = []
+
+        perm_ordering = torch.randperm(n) # potentially control for randomness here
+        for i in range(0, n, batch_size):
+            curr_indices = perm_ordering[i:i+batch_size]
+            X_mask_batch = masked_data[curr_indices]
+            X_unmask_batch = masked_data[curr_indices]
+
+            opt.zero_grad()
+            autoencoder.encoder.kl = 0
+
+            X_hat = autoencoder(X_mask_batch)
+            loss = ((X_unmask_batch - X_hat)**2).sum() + autoencoder.encoder.kl
+            loss = loss / batch_size
+            # loss = ((X_unmask_batch - X_hat)**2).sum() 
+            losses.append(loss.item())
+
+            loss.backward()
+            opt.step()
+    
+        avg_los_epoch.append(sum(losses)/len(losses))
+            
+    # print(avg_los_epoch)
+    print(f"Staring avg losses: {avg_los_epoch[0]}, {avg_los_epoch[1]}, {avg_los_epoch[2]}")
+    print(f"Ending avg loss: {avg_los_epoch[-1]}, {avg_los_epoch[-2]}, {avg_los_epoch[-3]}")
+    return autoencoder
